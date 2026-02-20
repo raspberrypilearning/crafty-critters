7f6bbc1ffadbef3c89dbd719da7ecb43191828a1
awaywiththespoons <becca.rose@raspberrypi.org>
2026-02-20 13:03:47 +0000

    image changes

--- en/code/crafty-critters-complete/main.py
+++ en/code/crafty-critters-complete/main.py
@@ -1,1 +1,25 @@
-print("Hello, world")
+from p5 import *
+from random import randint
+
+def setup():
+    size(400, 400)
+    image_mode(CENTER)
+    global body, eye, leg
+    body = load_image('body1.png')
+    leg = load_image('leg1.png')
+    eye = load_image('leg1.png')
+
+def draw():
+    background(220, 30, 124);
+    image(leg, 150, 320);
+    image(leg, 250, 300);
+    image(leg, 350, 320);
+    image(leg, 450, 330);
+    image(body, width / 2, 200, 500);
+    image(eye, 350, 120, 200);
+    image(eye, 500, 100, 200);
+
+run() # Keep this to run your code
+
+
+run() # Keep this to run your code
